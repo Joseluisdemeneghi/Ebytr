6f222646e1a9f7328a9bd048cc5e9941be4c40c0
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-02-15 18:06:32 +0000

    criaçao do index com configuraçao inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+});
