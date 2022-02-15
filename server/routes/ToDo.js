35d00dd6c952e8e44ef55d33330945f4fef4ed63
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-02-15 19:48:42 +0000

    adiciona get tasks a rota

--- server/routes/ToDo.js
+++ server/routes/ToDo.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/ToDoController');
 
 const router = express.Router();
 router.post('/', controller.create);
+router.get('/', controller.getTasks);
 
 module.exports = router;
