ab0c32b9e82b1d66128024c299455376559fba7b
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-02-16 18:07:01 +0000

    cria o formulario de task e valida o create e delete

--- to-do-client/src/components/Tasks.js
+++ to-do-client/src/components/Tasks.js
@@ -1,27 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { listTasks } from '../utils/services';
+import { listTasks, createTask, deleteTask } from '../utils/services';
 
 function Tasks() {
   const [tasksList, setTasksList] = useState();
-  console.log(tasksList);
+  const [task, setTask] = useState('');
+  const [date, setDate] = useState('');
+  const [status, setStatus] = useState('pendente');
+  
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await listTasks();
-        setTasksList(data);
-      console.log(data);
+      setTasksList(data);
     }
     
    fetchData();
   }, []);
 
+  function handleTask(event) {
+    setTask(event.target.value);
+  }
+
+  function handleDate(event) {
+    setDate(event.target.value);
+  }
+
+  function handleStatus(event) {
+    setStatus(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await createTask({ task, date, status })
+    setTask('');
+    setDate('');
+    setStatus('pendente');
+    const { data } = await listTasks();
+    setTasksList(data);
+  }
+
+  async function handleDelete(taskId) {
+    await deleteTask(taskId);
+    const { data } = await listTasks();
+    setTasksList(data);
+  }
+
   return (
     <div>
+      <div>
+        <input type="text" value={task} onChange={handleTask} placeholder="Tarefa" />
+        <input type="text" value={date} onChange={handleDate} placeholder="Data" />
+        <select name="status" id="status" value={status} onChange={handleStatus}>
+          <option value="pendente">pendente</option>
+          <option value="em andamento">em andamento</option>
+          <option value="pronto">pronto</option>
+        </select>
+        <button type='submit' onClick={handleSubmit}>Nova Tarefa</button>
+      </div>
       { tasksList && tasksList.map((task) => (
         <div key={task._id}>
           <p>{task.task}</p>
           <p>{task.date}</p>
           <p>{task.status}</p>
-          <button type='submit'>Delete</button>
+          <button type='submit' onClick={() => handleDelete(task._id)}>Delete</button>
         </div>
       ))}
     </div>
