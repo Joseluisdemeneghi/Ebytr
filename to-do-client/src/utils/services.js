165a8ab570f61348c2ad037fbe8adc78790cb131
José Demeneghi <josedemeneghi_cba@hotmail.com>
2022-02-16 10:14:49 +0000

    adiciona o axios e cria as funcoes de conexao com o db

---
+++ to-do-client/src/utils/services.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:3000/tasks';
+
+export function createTask(task) {
+  return axios.post(API_URL, task);
+}
+
+export function listTasks() {
+  return axios.get(API_URL);
+}
+
+export function updateTask(id, task) {
+  return axios.put(API_URL + "/" + id, task);
+}
+
+export function deleteTask(id) {
+  return axios.delete(API_URL + "/" + id);
+}
